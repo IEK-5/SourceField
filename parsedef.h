5d179d5fe16838f50c0b0b969b1d841a9cb57b03
bart <b.pieters@fz-juelich.de>
2013-11-05 12:32:28 +0100

    updated headers regarding copyright and GPL

--- parsedef.h
+++ parsedef.h
@@ -1,5 +1,5 @@
-/*****************************************************************               
- *  I N S T I T U T   F U E R   E N E R G I E F O R S C H U N G  *                 
+/*****************************************************************              
+ *  I N S T I T U T   F U E R   E N E R G I E F O R S C H U N G  *              
  *  I E K - 5   P H O T O V O L T A I K                          *              
  *                                                               *              
  *        ########                _   _                          *              
@@ -12,12 +12,26 @@
  *     ##      #######      F o r s c h u n g s z e n t r u m    *              
  *       ##########                                              *              
  *                                                               *              
- *                    http://www.fz-juelich.de/ief/ief5/index    *              
+ *   http://www.fz-juelich.de/iek/iek-5/EN/Home/home_node.html   *              
  *****************************************************************
  *                                                               *
- * Dr. Bart E. Pieters 2011                                      *
+ * Copyright (C) 2013 Dr. Bart E. Pieters                        *
  *                                                               *             
  *****************************************************************/                                                                             
+                                                                             
+/*  This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see {http://www.gnu.org/licenses/}.
+*/
 
 /* parse keys and keywords */
 
